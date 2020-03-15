f3b690fa2d9d8157c9661cabed2b8770726c09b6
Siyun Li <lisiyun96@gmail.com>
2020-03-15 01:09:01 -0400

    Update preprocessing script to augment training data with rotation and resizing

--- pre_processing.py
+++ pre_processing.py
@@ -8,6 +8,34 @@ data_in_path = os.path.join(os.getcwd(), 'gtsrb-german-traffic-sign')
 data_out_path = os.path.join(os.getcwd(), 'cleaned-dataset')
 
 category = [0, 1, 4, 6, 8, 9, 10, 12, 13, 15, 16, 17, 18, 22, 25, 27, 29, 32, 35, 36, 37, 38, 39, 40]
+target_dim = 48
+
+def rotate_image(img, angle):
+    M = cv2.getRotationMatrix2D((target_dim, target_dim), angle, 1)
+
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
+    n = int((target_dim * sin) + (target_dim * cos))
+
+    shift = (n / 2) - target_dim // 2
+    M[0, 2] += (angle > 0) * shift
+    M[1, 2] += (angle < 0) * shift
+
+    return cv2.warpAffine(img, M, (target_dim, target_dim))
+
+
+def downscale_image(img):
+    dim_new = 32
+
+    start = (target_dim - dim_new) // 2
+    end = start + dim_new
+
+    back = np.zeros((target_dim, target_dim, 3), dtype=np.uint8)
+    front = cv2.resize(img, (dim_new, dim_new))
+
+    back[start:end, start:end] = front
+    return back
+
 
 def clean_and_create_project_training_dataset():
     for i in range(len(category)):
@@ -20,9 +48,23 @@ def clean_and_create_project_training_dataset():
 
         for item in os.listdir(img_in_path):
             if str(item[-4:]) == '.png':
+
+                # resize image
                 img = cv2.imread(os.path.join(img_in_path, item))
                 img = cv2.resize(img, (48, 48))
                 cv2.imwrite(os.path.join(img_out_path, item), img)
+
+                # rotate ccw by 10
+                rotated = rotate_image(img, 10)
+                cv2.imwrite(os.path.join(img_out_path, item[:-4] + '_rotL.png' ), rotated)
+
+                # rotate cw by 10
+                rotated = rotate_image(img, -10)
+                cv2.imwrite(os.path.join(img_out_path, item[:-4] + '_rotR.png' ), rotated)
+
+                # downscale
+                smaller = downscale_image(img)
+                cv2.imwrite(os.path.join(img_out_path, item[:-4] + '_small.png' ), smaller)
         
         print("Finished...", img_in_path)
 
@@ -52,5 +94,5 @@ if __name__ == "__main__":
     if not os.path.exists(data_out_path):
         os.makedirs(data_out_path, exist_ok=True)
 
-    # clean_and_create_project_training_dataset()
+    clean_and_create_project_training_dataset()
     clean_and_create_project_test_dataset()
